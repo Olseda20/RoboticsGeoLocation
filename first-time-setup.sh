a8425a59db3de8d502d2c789691afa464a43dcb3
Omro <olseda20@bath.ac.uk>
2022-09-04 10:47:34 +0000

    creating a first-time-setup.sh file (still needs testing but should be a reasonable blueprint for now)

---
+++ first-time-setup.sh
@@ -0,0 +1,24 @@
+#! usr/bin/bash
+echo "Heyyo, this is the first time setup of my rosproject. This will install all the relevant dependancies to get an appropriate ros container set up."
+
+# getting ros set up
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo apt-get install build-essential libssl-dev
+export LIBGL_ALWAYS_INDIRECT=1
+sudo apt-get install -y mesa-utils libgl1-mesa-glx
+sudo apt-get install ros-noetic-ros
+sudo apt-get install ros-noetic-turtlesim
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+
+# creating the project file
+DIR=$(pwd)
+mkdir /project
+cd project
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+source /opt/ros/noetic/setup.bash
+catkin_make
+source devel/setup.bash
+echo $ROS_PACKAGE_PATH
+cd $DIR
