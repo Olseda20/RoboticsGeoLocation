0150e65cd473e28b8eb8148029dc792c419d10f5
Omro <olseda20@bath.ac.uk>
2022-09-05 11:58:14 +0000

    Renaming PositionCalculation file and using alias in run file

--- run.py
+++ run.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-from shutil import move
 import sys
 import time
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import rospy
 import TurtleMotion
-import PositionCalc
+import PositionCalculations as PoseCalc
 
 
 def pose_callback(pose_message):
@@ -22,14 +21,14 @@ def move_to_goal(destination_location, velocity_publisher):
     ''' method to move turtle to desired goal location '''
     # identify goal location
     # initialise distance and theta to goal
-    distance_to_goal, theta_to_goal = PositionCalc.evaluate_distance(destination_location, pose)
+    distance_to_goal, theta_to_goal = PoseCalc.evaluate_distance(destination_location, pose)
 
     # compare current location to the goal location
     while distance_to_goal >= 1:
-        distance_to_goal, theta_to_goal = PositionCalc.evaluate_distance(destination_location, pose)
+        distance_to_goal, theta_to_goal = PoseCalc.evaluate_distance(destination_location, pose)
 
-        updated_linear_velocity = PositionCalc.calculate_linear_velocity(distance_to_goal, 0.5)
-        updated_angular_velocity = PositionCalc.calculate_angular_velocity(theta_to_goal, pose.theta, 4)
+        updated_linear_velocity = PoseCalc.calculate_linear_velocity(distance_to_goal, 0.5)
+        updated_angular_velocity = PoseCalc.calculate_angular_velocity(theta_to_goal, pose.theta, 4)
 
         TurtleMotion.move_turtle(updated_linear_velocity, updated_angular_velocity, velocity_publisher, velocity)
         rospy.loginfo(distance_to_goal)
@@ -38,22 +37,31 @@ def move_to_goal(destination_location, velocity_publisher):
 
     rospy.loginfo('goal has been reached')
 
-if __name__ == "__main__":
+def main(goal_x = None, goal_y = None):
+    ''' main run script - to move turtle to a specified goal location '''
+    
+    if ((goal_x is None) or (goal_y is None)):
+        rospy.loginfo("Please prove a goal location in the to the method")
+        return 0
     # create turtle node
     rospy.init_node('TurtleNode', anonymous=True)
 
     # velocity publisher
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     # position subscriber
-    position_subscriber = rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
+    rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
     time.sleep(2)
 
     rate = rospy.Rate(10)
     velocity = Twist()
     pose = Pose()
 
-    move_to_goal([float(sys.argv[1]), float(sys.argv[2])], velocity_publisher)
+    move_to_goal([goal_x, goal_y], velocity_publisher)
+
+if __name__ == "__main__":
+    main(float(sys.argv[1]), float(sys.argv[2]))
+
     # while True:
     #     TurtleMotion.move_turtle(float(sys.argv[1]), float(sys.argv[2]), velocity_publisher, velocity)
-    #     # pose = PositionCalc.get_current_pose(pose)
+    #     # pose = PoseCalc.get_current_pose(pose)
     #     rospy.loginfo(pose)
