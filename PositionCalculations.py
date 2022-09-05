a8972f8157f12d2abe125c02e4c4c3f773b4981c
Omro <olseda20@bath.ac.uk>
2022-09-06 00:04:06 +0100

    moved motion methods to TurtleMotion file

--- PositionCalculations.py
+++ PositionCalculations.py
@@ -6,7 +6,7 @@ import time
 import rospy
 from turtlesim.msg import Pose
 
-POSE = None
+POSE = Pose()
 
 def initiate_velocity_publisher():
     ''' initations pose subscriber '''
@@ -18,7 +18,6 @@ def initiate_velocity_publisher():
     rospy.init_node('TurtleMotion', anonymous=True)
     rospy.Subscriber('/turtle1/pose', Pose, callback)
     time.sleep(2)
-
 
 def get_current_pose(pose):
     """ Callback function which is called when a new message of type Pose is received 
@@ -37,15 +36,6 @@ def evaluate_distance(goal_location, current_pose):
 
     return distance_to_goal, theta_to_goal
 
-def calculate_linear_velocity(distance_to_goal, k = 0.8):
-    ''' calculates the desired linear velovity with proprtional control (k) '''
-    linear_velocity = distance_to_goal*k
-    return linear_velocity
-
-def calculate_angular_velocity(theta_goal, theta, k = 9):
-    ''' calculates the desired angular velovity with proprtional control (k) '''
-    angular_velocity = (theta_goal - theta)*k
-    return angular_velocity
 
 def main():
     ''' main run method '''
